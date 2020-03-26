819e9cab33f729e8939ed398424ef93672c59cee
favour josh <FJ@Freeborn-MacBook-Pro.local>
2020-03-26 20:04:37 +0100

    java script

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let name = 'Egwuenu Favour';
+let course = ['frontend'];
+let person = {
+name: 'Egwuenu Favour',
+course: 'HTML,CSS,JAVASCRIPT'
+};
+console.log(person)
+
+function odd(num) {
+    for (let i=1; i<num; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+    }
+}
+
+odd(200);
